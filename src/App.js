76489cc903a764a3d1092ead70efe8488581bd04
Corey Miller <coreyMiller353@gmail.com>
2020-06-08 23:39:10 -0400

    add framework for sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import EntriesTable from "./components/table";
+import Sidebar from "./components/sidebar";
 
 function App() {
 	return (
 		<div>
+			<Sidebar />
 			<EntriesTable />
 		</div>
 	);
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Sidebar = () => {
+	return (
+		<div>
+			<ul>
+				<li>Item 1</li>
+				<li>Item 2</li>
+				<li>Item 3</li>
+				<li>Item 4</li>
+				<li>Item 5</li>
+				<li>Item 6</li>
+				<li>Item 7</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Sidebar;
